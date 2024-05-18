8c141bee253fcb31dacac333db032d1513588f2a
Halil777 <microsoft779@gmail.com>
2024-05-18 23:21:00 +0500

    background to background image

--- dist/assets/api-BbuOG2Kf.js
+++
@@ -1,1 +0,0 @@
-import{h as a}from"./index-BX1926oX.js";const t="http://95.85.121.153:1337",s=a.create({baseURL:t});export{s as a};
---
+++ dist/assets/api-G5PSLJVs.js
@@ -0,0 +1,1 @@
+import{h as a}from"./index-Bg-S2_6f.js";const t="http://95.85.121.153:1337",s=a.create({baseURL:t});export{s as a};
