94d52afb5e4cb6226e07ab094ff72ce160b2543d
Halil777 <microsoft779@gmail.com>
2024-09-05 19:42:37 +0500

    changed carousel for home xs

--- src/pages/home/components/SocialCardXS.tsx
+++ src/pages/home/components/SocialCardXS.tsx
@@ -1,20 +1,17 @@
-import { IconButton, Stack, Typography } from "@mui/material";
+import { Stack, Typography } from "@mui/material";
 import { FC, useState, useEffect, useRef } from "react";
-import ArrowRightAltIcon from "@mui/icons-material/ArrowRightAlt";
 import axios from "axios";
-import { Swiper, SwiperSlide } from "swiper/react";
-import { Autoplay } from "swiper/modules";
+import Slider from "react-slick";
 import { useTranslation } from "react-i18next";
 import { swiperStyleXS } from "../../../utils/swiper";
-import { arrowIconStyle } from "../../../common/style/commonStyle";
 
-import "swiper/css";
-import "swiper/css/autoplay";
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
 
 const SocialCardXS: FC = () => {
   const { i18n } = useTranslation();
   const [socialData, setSocialData] = useState<any[]>([]);
-  const swiperRef = useRef<any>(null); // Ref to control Swiper programmatically
+  const sliderRef = useRef(null); // Ref to control Slider programmatically
 
   useEffect(() => {
     const fetchSocialData = async () => {
@@ -32,9 +29,8 @@ const SocialCardXS: FC = () => {
   }, []);
 
   useEffect(() => {
-    if (swiperRef.current && swiperRef.current.swiper) {
-      // Manually start autoplay
-      swiperRef.current.swiper.autoplay.start();
+    if (sliderRef.current) {
+      (sliderRef.current as Slider).slickPlay(); // Cast to Slider type
     }
   }, [socialData]);
 
@@ -51,36 +47,36 @@ const SocialCardXS: FC = () => {
     }
   };
 
+  const settings = {
+    infinite: true,
+    speed: 1000,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    autoplay: true,
+    autoplaySpeed: 3000,
+    cssEase: "linear",
+    pauseOnHover: true,
+    arrows: false,
+  };
+
   return (
     <div style={{ width: "100%", display: "flex", justifyContent: "center" }}>
-      <Swiper
-        ref={swiperRef}
-        modules={[Autoplay]}
-        slidesPerView={1}
-        centeredSlides={true}
-        style={{ ...swiperStyleXS, position: "relative" }}
-        speed={1000}
-        loop={true}
-        autoplay={{
-          delay: 3000,
-          disableOnInteraction: false,
-          pauseOnMouseEnter: true, // Optional: Pause on hover
-        }}
-      >
-        {socialData.map((item) => (
-          <SwiperSlide key={item.id}>
-            <Stack
-              direction="row"
-              justifyContent="space-between"
-              sx={{
-                background: "#D9D9D9",
-                p: 1,
-                borderRadius: "8px",
-                mr: 1,
-                width: "90%",
-              }}
-            >
-              <Stack direction="row" spacing={3}>
+      <div style={{ ...swiperStyleXS, position: "relative" }}>
+        <Slider ref={sliderRef} {...settings}>
+          {socialData.map((item) => (
+            <div key={item.id}>
+              <Stack
+                onClick={() => window.open(item.url, "_blank")}
+                direction="row"
+                spacing={3}
+                sx={{
+                  background: "#D9D9D9",
+                  p: 1,
+                  borderRadius: "8px",
+                  mr: 2,
+                  width: "95%",
+                }}
+              >
                 <img
                   style={{
                     width: "80px",
@@ -105,22 +101,10 @@ const SocialCardXS: FC = () => {
                   }}
                 />
               </Stack>
-              <IconButton
-                sx={{
-                  ...arrowIconStyle,
-                  width: "20px",
-                  height: "20px",
-                  top: 10,
-                  right: 35,
-                }}
-                onClick={() => window.open(item.url, "_blank")}
-              >
-                <ArrowRightAltIcon />
-              </IconButton>
-            </Stack>
-          </SwiperSlide>
-        ))}
-      </Swiper>
+            </div>
+          ))}
+        </Slider>
+      </div>
     </div>
   );
 };
